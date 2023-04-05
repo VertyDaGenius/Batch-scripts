ef2227b087f84eb4c4be92e71b35e02d1aa845dc
AndrewTheSkid <121377353+AndrewTheSkid@users.noreply.github.com>
2023-04-05 13:19:00 -0700

    Password_Generator.bat
    this script generates a password and you choose the length

---
+++ Password_Generator.bat
@@ -0,0 +1,17 @@
+@echo off
+title password generator made by AndrewTheSkid
+
+  setlocal EnableDelayedExpansion
+   set /p passlength=Enter The Password Length:
+   set pwdchars=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}[]\/><.,`~
+
+   set pwd=
+     for /l %%# in (1,1,%passlength%) do (
+   set /a randomchar=!random! %% 53
+   set pwd=!pwd!!pwdchars:~%randomchar%,1!
+    )
+
+      echo Your password is:
+      echo %pwd%
+
+pause
