f88645f3c3e66aaf1aabebd0d4246f12fa16d1f8
Verty The Genius <121377353+VertyDaGenius@users.noreply.github.com>
2023-05-09 21:08:08 -0700

    Update PwnedVirus.bat

--- PwnedVirus.bat
+++ PwnedVirus.bat
@@ -1,4 +1,21 @@
 @echo off
+echo this is an almost harmless "virus" that spams different file directories
+echo i found this in my old usb drive as one of my first batch scripts
+echo if you dont want to run this script or if you accidently do,
+echo task kill the script or type in ctrl c inside the script to end it.
+echo to recover from this script if it was opened and ran for a while
+echo restart your computer then select every created file then delete it.
+echo if this doesnt work i am not responsible for any damages caused by this.
+pause
+echo are you sure?
+pause
+echo are you absolutely sure?
+pause
+echo ok you asked for it..
+pause
+echo LAST CHANCE!!!!!!!!
+pause
+cls
 title Pwned
 :pwned
    cmd
