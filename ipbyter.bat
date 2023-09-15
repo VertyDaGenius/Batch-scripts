5a45985cdb7e7e0bcbb5ce070be7f9b5b375ac0c
Verty The Genius <121377353+VertyDaGenius@users.noreply.github.com>
2023-09-15 12:49:09 -0700

    Update ipbyter.bat
    i havent tested this out but it should work

--- ipbyter.bat
+++ ipbyter.bat
@@ -1,6 +1,9 @@
 @echo off
 color 2
 title ip byter - by verty
+echo I do not take responsibility on any legal repercussions for the usage of this script, nor do I condone unethical hacking.
+timeout /t 2
+cls
 echo welcome to the ip byter!
 echo This script will take a byte out of an ip address.
 echo.
@@ -17,18 +20,32 @@ set byte=65500
 ping %ip% -n 1 -l %byte% | findstr /i "Reply" >nul
 if %errorlevel% == 0 (
     color 0A
-    echo %ip% is up.
-    echo bytes = %byte%
+    echo %ip% is up. -bytes = %byte%
+    echo.
     set /p question="Would you like to go back or keep pinging? type y to keep pinging or n to go back: "
-      if /I "%question%" EQU "Y" Goto iploop
-      if /I "%question%" EQU "y" Goto iploop
       if /I "%question%" EQU "N" Goto main
       if /I "%question%" EQU "n" Goto main
+      if /I "%question%" EQU "Y" Goto ping
+      if /I "%question%" EQU "y" Goto ping
+
 ) else (
     color 0C
-    echo %ip% is down. byting the ip...
-    echo bytes = %byte%
+    echo %ip% is down. byting the ip... bytes = %byte%
+    echo.
     set /a byte=byte-1
 )
 
 goto iploop
+
+:ping
+echo.
+set /p times="how many times would you like to ping?: "
+ping %ip% -n %times% -l %byte%
+
+if %errorlevel%==0 (
+    color 0A
+    echo %ip% is up.
+) else (
+    color 0C
+    echo %ip% is down.
+)
